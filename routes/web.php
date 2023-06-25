47501972489f0967f1f87be1de8262d3d5f1a68d
KHLIFMOHAMEDAMINE2001 <khlif.mohamedamine@ensi-uma.tn>
2023-06-25 20:30:29 +0200

    Alhamdulilleh! fin auth et gestion des roles !

--- routes/web.php
+++ routes/web.php
@@ -30,35 +30,35 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    
-    //Route::get('/commercial',[SubdomainController::class,'showForm'])->name('subdomain.showForm');
+});
 
+Route::middleware('auth','verified','role:commercial')->group(function () {
+    Route::get('/commercial',[SubdomainController::class,'showForm'])->name('subdomain.showForm');
+    Route::post('subdomain',[SubdomainController::class,'store'])->name('subdomain.store');
+    
+});
+
+Route::middleware('auth','verified','role:support')->group(function () {
     Route::get('/support/packages',[PackageController::class,'show'])->name('package.show');
     Route::get('/support/add_package',[PackageController::class,'showaddpackage'])->name('package.showaddpackage');
     Route::post('/support/add_package',[PackageController::class,'store'])->name('package.store');
     Route::delete('/support/packages/{id}', [PackageController::class, 'destroy'])->name('package.destroy');
+});
 
+Route::middleware('auth','verified','role:financier')->group(function () {
     Route::get('/finance/add_payment',[PaymentController::class,'showpayment'])->name('payment.showpayment');
     Route::get('/finance/add_payment',[PaymentController::class,'showinfo'])->name('payment.showinfo');
     Route::post('/finance/add_payment',[PaymentController::class,'store'])->name('payment.store');
 
     Route::get('/finance/payments',[PaymentController::class,'showlistpayment'])->name('payment.showlistpayment');
     Route::get('/finance/payments',[PaymentController::class,'show'])->name('payment.show');
-    Route::delete('/finance/payments/{id}', [PaymentController::class, 'destroy'])->name('payment.destroy');
+    Route::delete('/finance/payments/{id}', [PaymentController::class, 'destroy'])->name('payment.destroy'); 
 });
-
 
 //Route::middleware('auth', 'role:admin')->group(function () {
 //    Route::post('subdomain',[SubdomainController::class,'store'])->name('subdomain.store');
 //});
 
-Route::get('/commercial', function () {
-    return view('subdomain');
-})->middleware(['auth', 'verified','role:admin'])->name('dashboard');
-
-Route::get('/support/packages', function () {
-    return view('packages');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
