0a84d2de43c2d3eb85de4628c668c4e26614c1ae
Mayssa Ayachi <ayachi.mayssa@ensi-uma.tn>
2023-07-06 22:12:25 +0100

    i cry

--- database/migrations/tenant/2014_10_12_000000_create_userstenants_table.php
+++ database/migrations/tenant/2014_10_12_000000_create_userstenants_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+
+class CreateUserstenantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +14,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('userstenants', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('userstenants');
     }
 };
