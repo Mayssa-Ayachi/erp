1fa8b45222659a60b72d0150f0fdc93b0d530f75
Mayssa Ayachi <ayachi.mayssa@ensi-uma.tn>
2023-07-07 10:48:39 +0100

    emailllllllllllll

--- app/Http/Controllers/SubdomainController.php
+++ app/Http/Controllers/SubdomainController.php
@@ -6,6 +6,10 @@ use App\Models\Tenant;
 use App\Models\Packagedetails;
 use Illuminate\Http\Request;
 
+use App\Mail\Email;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
 Class SubdomainController extends Controller
 {
     public function store(Request $request){
@@ -14,6 +18,10 @@ Class SubdomainController extends Controller
         $tenant->domains()->create(['domain' => $request->subdomain .'.localhost']);
         $tenant->tenantpackages()->create(['tenant_package' => $request->input('package'),
         'tenant_email' => $request->input('email'),]);
+        $adress = $request->subdomain .'.localhost:8000/users/create';
+        $name = $request->subdomain;
+        $package = $request->input('package');
+        Mail::to($request->input('email'))->send(new Email($adress,$name,$package));
         session()->flash('success', 'The organization has been successfully created.');
         } catch (\Exception $e) {
             session()->flash('failed', 'An error occurred while creating the organization.');
---
+++ app/Mail/Email.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+
+class Email extends Mailable
+{
+    use Queueable, SerializesModels;
+    
+    public $adress;
+    public $name;
+    public $package;
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($adress,$name,$package)
+    {
+        $this->adress=$adress;
+        $this->name=$name;
+        $this->package=$package;
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Email',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            view: 'email',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
